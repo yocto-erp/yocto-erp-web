41d8b601fafa96cbda268d8bd64c9af414837a9c
lephuoccanh <lephuoccanh@gmail.com>
2020-08-17 01:44:18 +0700

    Add Bootstrap Admin template.

--- app/containers/dashboard/components/calendar/Day.js
+++ app/containers/dashboard/components/calendar/Day.js
@@ -20,72 +20,79 @@ class Day extends Component {
     const { day, selected } = this.props;
     return (
       <div
-        className={
-        `${s.day}` +
-          (day.isToday ? ` ${s.today}` : '') +
-          (day.isCurrentMonth ? '' : ` ${s.differentMonth}`) +
-          (day.date.isSame(selected) ? ` ${s.selected}` : '') +
-          (day.hasEvents ? ` ${s.hasEvents}` : '')
-        }
+        className={`${s.day}${day.isToday ? ` ${s.today}` : ''}${
+          day.isCurrentMonth ? '' : ` ${s.differentMonth}`
+        }${day.date.isSame(selected) ? ` ${s.selected}` : ''}${
+          day.hasEvents ? ` ${s.hasEvents}` : ''
+        }`}
       >
         {!day.hasEvents ? (
-          <div className={s.dayNumber}>{day.number}</div> 
-          : (day.hasEvents && day.link) 
-            ? 
-            <React.Fragment>
-              <a
+          <div className={s.dayNumber}>{day.number}</div>
+        ) : day.hasEvents && day.link ? (
+          <React.Fragment>
+            <a
               rel="noopener noreferrer"
-                target="_blank"
+              target="_blank"
               onMouseEnter={this.toggleTooltip}
               onMouseOut={this.toggleTooltip}
-                id={`Tooltip${day.number}`}
-                href={day.link ? day.link : "#"} 
-                className={s.dayNumber}> {day.number}
-                {day.itemStyle ? 
-                  <span 
-                    style={{backgroundColor: `${day.itemStyle}`}} 
-                    className={s.calendarDot}>
-                  </span> : "" }
-              </a>
-              <Tooltip 
-                placement="top" 
-                isOpen={this.state.tooltipShow} 
-                toggle={this.toggleTooltip} 
-                target={`Tooltip${day.number}`}>
-              {day.title}
-            </Tooltip>
-            </React.Fragment> 
-            : (day.hasEvents && !day.link)
-              ? 
-              <React.Fragment>
-                <div
-                  onClick={this.togglePopover}
-                  id={`Popover${day.number}`}
-                  className={s.dayNumber}
+              id={`Tooltip${day.number}`}
+              href={day.link ? day.link : '#'}
+              className={s.dayNumber}
             >
               {' '}
               {day.number}
-                  {day.itemStyle ? 
-                    <span 
-                      style={{backgroundColor: `${day.itemStyle}`}} 
-                      className={s.calendarDot}>
-                    </span> : "" }
-                </div>
-                <Popover
-                  placement="top"
-                  isOpen={this.state.popoverShow}
-                  target={`Popover${day.number}`}
-                  toggle={this.togglePopover}
+              {day.itemStyle ? (
+                <span
+                  style={{ backgroundColor: `${day.itemStyle}` }}
+                  className={s.calendarDot}
+                />
+              ) : (
+                ''
+              )}
+            </a>
+            <Tooltip
+              placement="top"
+              isOpen={this.state.tooltipShow}
+              toggle={this.toggleTooltip}
+              target={`Tooltip${day.number}`}
             >
-                  <PopoverHeader>{day.title}</PopoverHeader>
-                  <PopoverBody>{day.info}</PopoverBody>
-                </Popover>
-              </React.Fragment>
+              {day.title}
+            </Tooltip>
+          </React.Fragment>
+        ) : day.hasEvents && !day.link ? (
+          <React.Fragment>
+            <div
+              onClick={this.togglePopover}
+              id={`Popover${day.number}`}
+              className={s.dayNumber}
+            >
+              {' '}
+              {day.number}
+              {day.itemStyle ? (
+                <span
+                  style={{ backgroundColor: `${day.itemStyle}` }}
+                  className={s.calendarDot}
+                />
+              ) : (
+                ''
+              )}
+            </div>
+            <Popover
+              placement="top"
+              isOpen={this.state.popoverShow}
+              target={`Popover${day.number}`}
+              toggle={this.togglePopover}
+            >
+              <PopoverHeader>{day.title}</PopoverHeader>
+              <PopoverBody>{day.info}</PopoverBody>
+            </Popover>
+          </React.Fragment>
         ) : (
-              : "" }
+          ''
         )}
       </div>
     );
   }
 }
+
 export default Day;
