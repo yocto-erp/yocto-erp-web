eb29732fd268efde3e3b177e0780ef93ce23ced6
duynt4899 <duynt@bap.jp>
2020-10-21 21:43:46 +0700

    - update

--- app/app.js
+++ app/app.js
@@ -7,7 +7,10 @@
 
 // Needed for redux-saga es6 generator support
 import '@babel/polyfill';
-
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/stable';
+import 'fast-text-encoding/text';
 // Import all the third party stuff
 import React from 'react';
 import ReactDOM from 'react-dom';
