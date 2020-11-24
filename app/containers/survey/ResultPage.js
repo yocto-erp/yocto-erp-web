76c51b35a38a030a32e00fa947e96bc16459fded
lephuoccanh <lephuoccanh@gmail.com>
2020-11-24 22:49:25 +0700

    Fix CSS and update for Answers.
    Merge branch 'master' of github.com:yocto-erp/yocto-erp-web
    
     Conflicts:
    	app/containers/survey/index.js
    	app/libs/apis/survey.api.js

--- app/containers/survey/ResultPage.js
+++ app/containers/survey/ResultPage.js
@@ -18,23 +18,50 @@ const ResultPage = () => {
   return resp ? (
     <div className="d-flex h-100 justify-content-center">
       <div className="container p-4 text-center">
-        <div className="question-form">
-          <h1 className="mb-5">
-            {resp.survey.name}
-            <br />
-            <small className="text-muted">{resp.survey.remark}</small>
-          </h1>
-          {resp.answers.map(t => (
-            <div className="row" key={`${resp.id}-${t.question.id}`}>
-              <div className="col">
-                <PreviewQuestionAnswer
-                  question={t.question}
-                  answer={t.answer}
-                  isOpen
-                />
+        <h1 className="mb-5">
+          {resp.survey.name}
+          <br />
+          <small className="text-muted">{resp.survey.remark}</small>
+        </h1>
+        <div className="row">
+          <div className="col-md-6">
+            {resp.answers.map(t => (
+              <div className="row" key={`${resp.id}-${t.question.id}`}>
+                <div className="col">
+                  <PreviewQuestionAnswer
+                    question={t.question}
+                    answer={t.answer}
+                    isOpen
+                  />
+                </div>
               </div>
+            ))}
+          </div>
+          <div className="col-md-6 text-left mt-2">
+            <div className="alert alert-success text-nowrap text-ellipsis">
+              <strong>Ethereum Transaction:&nbsp;</strong>
+              {resp.blockchainId ? (
+                <a
+                  target="_blank"
+                  href={`https://ropsten.etherscan.io/tx/${resp.blockchainId}`}
+                >
+                  ${resp.blockchainId}
+                </a>
+              ) : (
+                <span>Processing ...</span>
+              )}
             </div>
-          ))}
+            <div className="alert alert-primary">
+              <strong>IPFS Hash:&nbsp;</strong>
+              {resp.ipfsId ? (
+                <a target="_blank" href={`https://ipfs.io/ipfs/${resp.ipfsId}`}>
+                  ${resp.ipfsId}
+                </a>
+              ) : (
+                <span>Processing ...</span>
+              )}
+            </div>
+          </div>
         </div>
       </div>
     </div>
