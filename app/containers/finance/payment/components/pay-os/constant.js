b2c42589debb1ee72d98ff3d8d20744558983c42
Le Phuoc Canh <lephuoccanh@gmail.com>
2024-01-19 00:04:50 +0700

    Add FormViewPage public.

--- app/containers/finance/payment/components/pay-os/constant.js
+++ app/containers/finance/payment/components/pay-os/constant.js
@@ -4,5 +4,6 @@ export const yupPayOS = Yup.object({
   payos: Yup.object({
     clientId: Yup.string().required("Client ID required"),
     apiKey: Yup.string().required("Api Key required"),
+    checksum: Yup.string().required("Checksum required"),
   }).required(),
 });
