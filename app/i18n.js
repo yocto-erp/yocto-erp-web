177f897b9ee323f36bfd4aaef25d0d992919c7e9
Le Phuoc Canh <lephuoccanh@gmail.com>
2026-06-24 16:03:10 +0700

    Test payment.

--- app/i18n.js
+++ app/i18n.js
@@ -1,7 +1,7 @@
 /**
  * i18n.js
  *
- * This will setup the i18n language files and locale data for your app.
+ * This will set up the i18n language files and locale data for your app.
  *
  *   IMPORTANT: This file is used by the internal build
  *   script `extract-intl`, and must use CommonJS module syntax
